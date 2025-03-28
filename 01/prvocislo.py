ee0cba2d2a3268a354118ff2b07ab72ae907fdaa
Jana D <drazkova.j@gmail.com>
2025-03-28 15:53:31 +0100

    Pořešen výstup pro hodnoty 1 a 2. Přidáno else k cyklu for

--- 01/prvocislo.py
+++ 01/prvocislo.py
@@ -4,20 +4,15 @@ import sys
 # one of larger prime numbers is f.e. 10657331232548839
 user_num = int(sys.argv[1])
 
-control = True
-
 # parity check
-if user_num%2 ==0:				
+if user_num%2 ==0 and user_num>2 or user_num==1:				
     print(f"Číslo {user_num} není prvočíslo.")
-    control = False
 # algorithm for odd numbers    
 else:
     for i in range(3,round(sqrt(user_num))+1,2):
         if user_num%i ==0:
             print(f"Číslo {user_num} není prvočíslo.")
-            control = False
             break
-
-if control:
-    print(f"Číslo {user_num} je prvočíslo.")
+    else:
+        print(f"Číslo {user_num} je prvočíslo.")
     
