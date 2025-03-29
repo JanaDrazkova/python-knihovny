06b22c115ac5bd833add24a92e9de6dabb219d18
Jana D <drazkova.j@gmail.com>
2025-03-29 13:32:29 +0100

    Kontrola vstupních hodnot, funkce is_prime_number

--- 01/prvocislo.py
+++ 01/prvocislo.py
@@ -2,17 +2,31 @@ from math import sqrt
 import sys
 
 # one of larger prime numbers is f.e. 10657331232548839
-user_num = int(sys.argv[1])
+
+def is_prime_number(num):
+    if num%2 ==0 and num>2 or num==1:				
+       return False
+    # algorithm for odd numbers    
+    else:
+        for i in range(3,round(sqrt(user_num))+1,2):
+            if user_num%i ==0:
+               return False
+        return True
+    
+
+try:
+    user_num = int(sys.argv[1])
+except ValueError:
+    print(f"Zadané číslo musí být přirozené.")
+    exit()
+
+#non_positive integers
+if user_num < 1:
+    raise ValueError(f"Zadané číslo musí být přirozené.")
 
 # parity check
-if user_num%2 ==0 and user_num>2 or user_num==1:				
-    print(f"Číslo {user_num} není prvočíslo.")
-# algorithm for odd numbers    
+if is_prime_number(user_num):
+    print(f'{user_num} je prvočíslo.')
 else:
-    for i in range(3,round(sqrt(user_num))+1,2):
-        if user_num%i ==0:
-            print(f"Číslo {user_num} není prvočíslo.")
-            break
-    else:
-        print(f"Číslo {user_num} je prvočíslo.")
+    print(f'{user_num} není prvočíslo.')
     
